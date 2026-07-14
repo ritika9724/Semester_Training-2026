ac6f6af1baf05689bcd24e7a38ac9ef1e6182f67
Java <ranikusum972@gmail.com>
2026-07-14 21:43:04 +0530

    STL List in C++

---
+++ DSA/day07/STL.list.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<list>
+#include<algorithm>
+using namespace std;
+
+                    // in list indexing is not work
+int main(){
+
+    list<int> l;
+    
+    l.push_back(20);
+    l.push_back(30);
+    l.push_front(10);
+    l.push_front(5);
+
+
+    cout<<"Original "<<endl;
+}
